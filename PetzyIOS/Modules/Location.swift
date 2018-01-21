cd4146ab0b9096524ff5ec63ddf2efe13d47374b
user <just_beta@hotmail.com>
2018-01-21 23:40:20 +0200

    Added Modules

---
+++ PetzyIOS/Modules/Location.swift
@@ -0,0 +1,9 @@
+//
+//  Location.swift
+//  PetzyIOS
+//
+//  Created by user on 1/20/18.
+//  Copyright © 2018 user. All rights reserved.
+//
+
+import Foundation
---
+++ PetzyIOS/Modules/Park.swift
@@ -0,0 +1,44 @@
+//
+//model class for new park with the following verables
+//title of the park
+//address of the park
+//user to idenify if park for sumbited or premade
+//image of the park from google maps
+//camera with the x and y axis
+//location of the current park with lat and lang
+//
+
+import Foundation
+
+class Park{
+    
+    //title of the park
+    var title : String
+    
+    //address of the park
+    var address : String
+    
+    //used to identify if park was sumbited by user
+    var user : String
+    
+    //image of the park from google maps
+    var image : String
+    
+    //camera x and y
+    var camera : String
+    
+    //lat lang location
+    var location : Location
+    
+    
+    //create class init
+    init(title : String,address : String,user : String,image : String,camera : String,
+         location : Location) {
+        self.title = tittle
+        self.address = address
+        self.user = user
+        self.image = image
+        self.camera = camera
+        self.location = location
+    }
+}
