051c18cae3b2cca07681758361d8ac3a7b7d0f04
LeoProk <just_beta@hotmail.com>
2018-01-26 23:08:53 +0200

    added fireBase database handling

--- PetzyIOS/Main/ParkListMaker.swift
+++ PetzyIOS/Main/ParkListMaker.swift
@@ -1,18 +1,26 @@
 //
-//  ParkListMaker.swift
-//  PetzyIOS
+//this class acsses firebase database from url to get the park information
 //
-//  Created by user on 1/20/18.
-//  Copyright © 2018 user. All rights reserved.
+//
 //
 
 import Foundation
+import Firebase
 
-class ParkListMaker {
+public class ParkListMaker {
     
-    var mParksList : Array<Park>
+    var ref: DatabaseReference!
     
     func getParks() -> Array<Park> {
-        
+        let listOfPark = Array<Park>()
+        // create firebase database reffarance from url
+        ref = Database.database().reference(fromURL: "https://petzy-1001.firebaseio.com/input")
+        //get the date from server in postDict
+        ref.observe(DataEventType.value, with: { (snapshot) in
+            let postDict = snapshot.value as? [String : AnyObject] ?? [:]
+            print(postDict)
+        })
+        // return array of parks
+        return listOfPark
     }
 }
