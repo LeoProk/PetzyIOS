39021041a159e7197125ccd67f7233f62900bbd1
LeoProk <just_beta@hotmail.com>
2018-02-15 23:55:30 +0200

    added location handling

--- PetzyIOS/ParkTableView/ParkTableViewController.swift
+++ PetzyIOS/ParkTableView/ParkTableViewController.swift
@@ -9,28 +9,49 @@
 import UIKit
 import SDWebImage
 import Firebase
+import SwiftLocation
+import MapKit
 
 class ParkTableViewController: UITableViewController {
 
     //empty array of parks
     var parksArray : Array<Park>!
-    
+    var curLoc : CLLocation!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //initalize array
         parksArray = []
-         let dispatchGroup = DispatchGroup()
+        //tries to get current location using swiftlocation
+        Locator.currentPosition(
+            //acuracy of cuuren location
+            accuracy: .house,
+            //timeout to look for lcoation
+            timeout: Timeout.delayed(60.0),
+            //if current location found set curLoc to current location
+            onSuccess: { 
+                cllocation in self.curLoc = cllocation
+        },
+            //on fail prin the error
+            onFail: { locationError, cllocation in
+               print(locationError)
+        })
+        //create dispatch group to update the tab view when done fetching all the
+        //data from firebase
+        let dispatchGroup = DispatchGroup()
         //populate the array with parks from firebase
         let ref = Database.database().reference(fromURL: "https://petzy-1001.firebaseio.com/input")
         dispatchGroup.enter()
         ref.observe(DataEventType.value, with: { (snapshot) in
             _ = snapshot.value as? [String : AnyObject] ?? [:]
-            //for loop that interact over snapshot data to poulate new park
+            //for loop that loops over snapshot data to poulate new park
             //add the park to parks array
             for fireObj in snapshot.children{
                 //create new park from the firebase info
                 let parkSnap = Park(dataSnap : fireObj as! DataSnapshot)
                 //adds the park to the park list
+                //if there still parks left continue to loop otherwise
+                //end the dispatch
                 if self.parksArray.count < snapshot.childrenCount - 1{
                     self.parksArray.append(parkSnap)
                 }else {
@@ -38,16 +59,13 @@ class ParkTableViewController: UITableViewController {
                 }
             }
         })
+        //when done looping over all the park reload the table view on
+        //main thread
         dispatchGroup.notify(queue: DispatchQueue.main, execute: {
            DispatchQueue.main.async {
                 self.tableView.reloadData()
             }
         })
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     override func didReceiveMemoryWarning() {
@@ -81,8 +99,10 @@ class ParkTableViewController: UITableViewController {
         cell.parkName.text = park.title
         //assign the park address to UILabel of the same category
         cell.parkAddress.text = park.address
+        //create new park location with the lat lng location from firebase
+        let parkLoc = CLLocation(latitude: Double(park.location.lat ?? "")!, longitude:Double(park.location.lng ?? "")!)
         //assogn the park location to UILabel of the same category
-        //cell.parkRange.text = park.location
+        cell.parkRange.text = distanceKM(distanceMeters: Int(curLoc.distance(from: parkLoc)))
         return cell
     }
     
@@ -91,49 +111,20 @@ class ParkTableViewController: UITableViewController {
     {
         return 400.0;
     }
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    //take distance in meters and convert it to kelomters
+    func distanceKM(distanceMeters:Int) -> String {
+        var range : String
+        var finalDistance : Int
+        //if the distance is less the 1 km show meters
+        if distanceMeters < 1000{
+            range = "מטרים"
+            finalDistance = distanceMeters
+        } else {
+            //if the distance is bigger the 1 km show kilometers
+            range = "קילומטרים"
+            finalDistance = distanceMeters / 1000
+        }
+        //return the distance and approparate range
+        return "\(finalDistance) \(range)"
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
